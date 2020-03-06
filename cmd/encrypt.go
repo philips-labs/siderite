18935bb770e17e6b48fd49e9a44c768200dcccbd
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-03-06 10:28:12 +0100

    Fix encrypt command

--- cmd/encrypt.go
+++ cmd/encrypt.go
@@ -1,24 +1,3 @@
-/*
-Copyright © 2020 Andy Lo-A-Foe <andy.lo-a-foe@philips.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package cmd
 
 import (
@@ -41,28 +20,49 @@ var encryptCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(encryptCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	encryptCmd.Flags().String("pubkey", "pubkey", "File containing the public key in PEM format")
-	encryptCmd.Flags().String("infile", "-", "Input file. Default is standard input")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// encryptCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	encryptCmd.Flags().StringP("keyfile", "k", "", "public key. Looks in ~/.iron.json otherwise")
+	encryptCmd.Flags().StringP("infile", "i", "", "input file. Default is standard input")
 }
 
 func encrypt(cmd *cobra.Command, args []string) {
-	config, err := iron.LoadConfig()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
 	var key string
-	if key = config.ClusterInfo[0].Pubkey; key == "" {
-		fmt.Println("missing public key in configuration")
-		return
+	var err error
+
+	// Key
+	keyFile, _ := cmd.Flags().GetString("pubkey")
+	if keyFile != "" {
+		keyBytes, err := ioutil.ReadFile(keyFile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		key = string(keyBytes)
+	} else { // Read from ~/.iron.json
+		config, err := iron.LoadConfig()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if key = config.ClusterInfo[0].Pubkey; key == "" {
+			fmt.Println("missing public key in configuration")
+			return
+		}
 	}
-	payload, err := ioutil.ReadAll(os.Stdin)
+
+	// Input
+	inFile, _ := cmd.Flags().GetString("infile")
+	var input *os.File
+	if inFile == "" {
+		input = os.Stdin
+	} else {
+		input, err = os.Open(inFile)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer input.Close()
+	}
+	payload, err := ioutil.ReadAll(input)
 	if err != nil {
 		fmt.Println(err)
 		return
