07f6a231b8f76750d72d61a26cf68d095b3adc42
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-03-21 09:22:25 +0100

    Add function mode. Use chisel for this.

---
+++ cmd/function.go
@@ -0,0 +1,81 @@
+package cmd
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	chclient "github.com/jpillora/chisel/client"
+	"github.com/jpillora/chisel/share/cos"
+	"github.com/spf13/cobra"
+)
+
+// functionCmd represents the function command
+var functionCmd = &cobra.Command{
+	Use:   "function",
+	Short: "Run in function mode",
+	Long:  `Runs siderite in hsdp_functio support mode`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Start
+		go chiselClient(args)
+		time.Sleep(2 * time.Second)
+		run(cmd, args)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(functionCmd)
+}
+
+func chiselClient(args []string) {
+	flags := flag.NewFlagSet("client", flag.ContinueOnError)
+	config := chclient.Config{Headers: http.Header{}}
+	flags.StringVar(&config.Fingerprint, "fingerprint", "", "")
+	flags.StringVar(&config.Auth, "auth", "", "")
+	flags.DurationVar(&config.KeepAlive, "keepalive", 25*time.Second, "")
+	flags.IntVar(&config.MaxRetryCount, "max-retry-count", -1, "")
+	flags.DurationVar(&config.MaxRetryInterval, "max-retry-interval", 0, "")
+	flags.StringVar(&config.Proxy, "proxy", "", "")
+	flags.StringVar(&config.TLS.CA, "tls-ca", "", "")
+	flags.BoolVar(&config.TLS.SkipVerify, "tls-skip-verify", false, "")
+	flags.StringVar(&config.TLS.Cert, "tls-cert", "", "")
+	flags.StringVar(&config.TLS.Key, "tls-key", "", "")
+	hostname := flags.String("hostname", "", "")
+	pid := flags.Bool("pid", false, "")
+	verbose := flags.Bool("v", false, "")
+	flags.Parse(args)
+	//pull out options, put back remaining args
+	args = flags.Args()
+	if len(args) < 2 {
+		log.Fatalf("A server and least one remote is required")
+	}
+	config.Server = args[0]
+	config.Remotes = args[1:]
+	//default auth
+	if config.Auth == "" {
+		config.Auth = os.Getenv("AUTH")
+	}
+	//move hostname onto headers
+	if *hostname != "" {
+		config.Headers.Set("Host", *hostname)
+	}
+	//ready
+	c, err := chclient.NewClient(&config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.Debug = *verbose
+	if *pid {
+		//generatePidFile()
+	}
+	go cos.GoStats()
+	ctx := cos.InterruptContext()
+	if err := c.Start(ctx); err != nil {
+		log.Fatal(err)
+	}
+	if err := c.Wait(); err != nil {
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/fatih/color v1.9.0
 	github.com/iron-io/iron_go3 v0.0.0-20190916120531-a4a7f74b73ac
 	github.com/jeffh/go.bdd v0.0.0-20120717032931-88f798ee0c74 // indirect
+	github.com/jpillora/chisel v1.7.6
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pelletier/go-toml v1.6.0 // indirect
 	github.com/pkg/errors v0.8.0
@@ -16,8 +17,10 @@ require (
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.4.0
-	golang.org/x/sys v0.0.0-20200219091948-cb0a6d8edb6c // indirect
-	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670 // indirect
+	golang.org/x/net v0.0.0-20210316092652-d523dce5a7f4 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 	gopkg.in/ini.v1 v1.52.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
